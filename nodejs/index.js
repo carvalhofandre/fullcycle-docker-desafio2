86d2c755e6c7c212bdf1d0bb966b7db00b113cb9
Andre Fagundes Carvalho <andrefagundescarvalho@gmail.com>
2024-04-04 16:04:25 -0300

    Implemented showing mysql data on browser

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs')
+
 const port = 5000
+const config = {
+    host: 'nodedb',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
 app.get('/', (req,res) => {
-    res.send('<h1>FullCycle Rocks!</h1>')
+    const sql = `SELECT * FROM people`
+    connection.query(sql, function (err, result, fields) {
+        if (err) {
+            res.render('profile', { data: '' })
+        } else {
+            res.render('profile', { data: result })
+        }
+    });
+    
 })
 
 app.listen(port, ()=>{
